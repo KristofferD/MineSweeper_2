ec9d92bdafeeb0d8f874f2b37f7cd8b6da19327f
Kristoffer Danbrant <krda15@student.bth.se>
2023-03-28 15:42:06 +0200

    BUGS!

--- Form1.cs
+++ Form1.cs
@@ -10,46 +10,32 @@ namespace MineSweeperAgain
         public Form1()
         {
             InitializeComponent();
-            _game = new Game(10, 10, 10);
-            dataGridView1.RowCount = _game.Board.NumRows;
-            dataGridView1.ColumnCount = _game.Board.NumCols;
+
+            gameBoard.RowCount = 10;
+            gameBoard.ColumnCount = 10;
+
+            _game = new Game(gameBoard.RowCount, gameBoard.ColumnCount, 10);
+
+            gameBoard.CellClick += GameBoard_CellClick;
         }
 
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        private void GameBoard_CellClick(object? sender, DataGridViewCellEventArgs e)
+
         {
-            int row = e.RowIndex;
-            int col = e.ColumnIndex;
-
-            if (e.Button == MouseButtons.Left)
+            private void GameBoard_CellClick(object sender, DataGridViewCellEventArgs e)
             {
-                _game.RevealCell(row, col);
-            }
-            else if (e.Button == MouseButtons.Right)
-            {
-                _game.ToggleFlaggedCell(row, col);
-            }
-
-            UpdateGridView();
-        }
-
-        private void UpdateGridView()
-        {
-            for (int row = 0; row < _game.Board.NumRows; row++)
-            {
-                for (int col = 0; col < _game.Board.NumCols; col++)
+                var mouseEventArgs = e as MouseEventArgs;
+                if (mouseEventArgs?.Button == MouseButtons.Left)
                 {
-                    var cell = _game.Board.GetCell(row, col);
-                    var dataGridViewCell = dataGridView1.Rows[row].Cells[col];
-                    if (cell.IsRevealed)
-                    {
-                        dataGridViewCell.Value = cell.IsMine ? "X" : cell.AdjacentMinesCount.ToString();
-                    }
-                    else
-                    {
-                        dataGridViewCell.Value = cell.IsFlagged ? "F" : "";
-                    }
+                    // Left click on cell
+                }
+                else if (mouseEventArgs?.Button == MouseButtons.Right)
+                {
+                    // Right click on cell
                 }
             }
+
+
         }
     }
 }
